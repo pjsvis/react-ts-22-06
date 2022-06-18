080aa0620d591a433f69f63879215e6832d673f1
Peter John Smith <pjstarifa@gmail.com>
2022-06-18 08:30:53 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>dev-dependencies</h2>
+      <ol>
+        <li>prettier</li>
+        <li>esling-wesbos</li>
+      </ol>
+    </div>
+  );
+}
