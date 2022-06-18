85bc28cd2173920ffac1eaab5a326ec42cb40344
Peter John Smith <pjstarifa@gmail.com>
2022-06-18 08:41:16 +0100

    Create deep-thought.ts

---
+++ src/deep-thought.ts
@@ -0,0 +1,26 @@
+import { removeSpaces } from "./string-helpers";
+
+const delay = (time: number) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+const strAnswers: string[] = ["42", "fortytwo"];
+
+const getValidityAsync = async (answer: unknown) => {
+  await delay(500);
+  if (typeof answer === "string" && removeSpaces(answer) === "42") {
+    return true;
+  }
+  if (
+    typeof answer === "string" &&
+    strAnswers.includes(removeSpaces(answer).toLowerCase())
+  ) {
+    return true;
+  }
+  return false;
+};
+
+export interface AnswerApi {
+  question: string;
+  getValidityAsync(answer: unknown): void;
+}
