f55bbe484ff930d16ce990f5e2ecf75cd032d126
Peter John Smith <pjstarifa@gmail.com>
2022-06-19 22:06:14 +0100

    Refactor more

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,14 @@ import { storeApi, useStoreState } from "./store";
 import "./styles.css";
 
 const divStyle = { marginBottom: "1em" };
-const getAnswer = (answer: string, validity: boolean) => {
-  if (answer.length === 0) {
-    return "";
-  }
-  return validity
-    ? `{answer} is the correct answer`
-    : `{answer} is the wrong answer`;
-};
+
 export default function App() {
   const [storeState] = useStoreState();
   const handleAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
     const answer = e.currentTarget.value;
     storeApi.setAnswer(answer);
   };
-  const { question, answer, validity } = storeState;
+  const { question, conclusion } = storeState;
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
@@ -26,7 +19,7 @@ export default function App() {
       <div style={divStyle}>
         <input type="text" onChange={handleAnswer} />
       </div>
-      <div>{getAnswer(answer, validity)}</div>
+      <div>{conclusion}</div>
     </div>
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -6,25 +6,31 @@ export interface Store {
   question: string;
   answer: string;
   validity: boolean;
+  conclusion: string;
 }
 
 const storeDefaults: Store = {
   question: answerApi.question,
   answer: "",
-  validity: false
+  validity: false,
+  conclusion: ""
 };
 const store = proxy(storeDefaults);
 
 interface StoreApi {
   setAnswer: (answer: string) => void;
 }
+
 const setAnswer = async (answer: string) => {
-  store.answer = answer;
+  store.answer = answer.length > 0 ? answer : "";
   const [err, res] = await to(answerApi.setValidityAsync(answer));
   if (err || !res) {
     store.validity = false;
+    store.conclusion = `${answer} is the wrong answer`;
     return;
   }
+  store.conclusion = `
+  ${answer} is the correct answer`;
   store.validity = res;
 };
 
